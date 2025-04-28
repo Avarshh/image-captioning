db9e2327f89a5ff727572325b1bee978a31fb26a
Avarshh <122387031+Avarshh@users.noreply.github.com>
2025-04-28 18:31:35 +0530

    yolort.py

--- yolort.py
+++ yolort.py
@@ -1,13 +1,10 @@
 import cv2
 from ultralytics import YOLO
 
-# Load YOLOv8 model (you can change to yolov8s.pt or yolov8x.pt for better accuracy)
-model = YOLO("yolov8n.pt")  # Nano = light & fast; use 'yolov8x.pt' for better detection
+model = YOLO("yolov8n.pt") 
+cap = cv2.VideoCapture(0)  
 
-# Start webcam
-cap = cv2.VideoCapture(0)  # Use 0 for default camera
 
-# Check camera status
 if not cap.isOpened():
     print("Error: Could not open webcam.")
     exit()
@@ -18,16 +15,13 @@ while True:
         print("Failed to grab frame")
         break
 
-    # Resize frame for speed (optional)
+   
     frame = cv2.resize(frame, (640, 480))
 
-    # Run object detection
     results = model(frame)
 
-    # Draw boxes and labels
     annotated_frame = results[0].plot()
 
-    # Show the result
     cv2.imshow("YOLOv8 Live Detection", annotated_frame)
 
     # Press 'q' to quit
